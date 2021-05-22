4d4c5355d7e024d2f8ee0d185cc0f3ed4c69d472
Susannie <zuzanna.maczek@gmail.com>
2021-05-22 22:36:57 +0200

    cleanup

--- maker/src/main/java/quizum/maker/QuizumMaker.java
+++ maker/src/main/java/quizum/maker/QuizumMaker.java
@@ -27,135 +27,145 @@ import quizum.QuizumUtils;
 import quizum.beans.Question;
 
 public class QuizumMaker {
-	private JFrame frmQuizumMaker;
-	private DefaultListModel<Question> listmodel;
-	private final JsonQuestionService jsonQuestionService = new JsonQuestionService();
-	private final CsvQuestionService csvQuestionService = new CsvQuestionService();
+    private JFrame frmQuizumMaker;
+    private DefaultListModel<Question> listmodel;
+    private final JsonQuestionService jsonQuestionService = new JsonQuestionService();
+    private final CsvQuestionService csvQuestionService = new CsvQuestionService();
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-					UIManager.getLookAndFeelDefaults().put("defaultFont", new Font(Configs.getInstance().getProperty("maker.font.family","SansSerif"), Integer.parseInt(Configs.getInstance().getProperty("maker.font.style","0")), Integer.parseInt(Configs.getInstance().getProperty("maker.font.size","12"))));
-					QuizumMaker window = new QuizumMaker();
-					window.frmQuizumMaker.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+                UIManager.getLookAndFeelDefaults().put("defaultFont", new Font(Configs.getInstance().getProperty("maker.font.family", "SansSerif"), Integer.parseInt(Configs.getInstance().getProperty("maker.font.style", "0")), Integer.parseInt(Configs.getInstance().getProperty("maker.font.size", "12"))));
+                QuizumMaker window = new QuizumMaker();
+                window.frmQuizumMaker.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
-	public QuizumMaker() {
-		initialize();
-	}
+    public QuizumMaker() {
+        initialize();
+    }
 
-	private void initialize() {
-		frmQuizumMaker = new JFrame();
-		frmQuizumMaker.setTitle("Quizum Maker");
-		frmQuizumMaker.setBounds(100, 100, 805, 440);
-		frmQuizumMaker.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmQuizumMaker.getContentPane().setLayout(null);
+    private void initialize() {
+        frmQuizumMaker = new JFrame();
+        frmQuizumMaker.setTitle("Quizum Maker");
+        frmQuizumMaker.setBounds(100, 100, 805, 440);
+        frmQuizumMaker.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmQuizumMaker.getContentPane().setLayout(null);
 
-		final JFileChooser fc = new JFileChooser();
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON Files", "json");
-		fc.setFileFilter(filter);
+        final JFileChooser fc = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Data Files", "json", "csv");
+        fc.setFileFilter(filter);
 
-		listmodel = new DefaultListModel<Question>();
-		JList<Question> list = new JList<Question>(listmodel);
-		list.setBounds(10, 36, 518, 334);
-		frmQuizumMaker.getContentPane().add(list);
+        listmodel = new DefaultListModel<>();
+        JList<Question> list = new JList<>(listmodel);
+        list.setBounds(10, 36, 518, 334);
+        frmQuizumMaker.getContentPane().add(list);
 
-		JLabel lblListaPyta = new JLabel("Lista pyta\u0144");
-		lblListaPyta.setBounds(10, 11, 114, 14);
-		frmQuizumMaker.getContentPane().add(lblListaPyta);
+        JLabel lblListaPyta = new JLabel("Lista pyta\u0144");
+        lblListaPyta.setBounds(10, 11, 114, 14);
+        frmQuizumMaker.getContentPane().add(lblListaPyta);
 
-		JButton btnNewButton = new JButton("Nowe pytanie");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CreateEditDialog newQuestionDialog = new CreateEditDialog();
-				Question newQuestion = newQuestionDialog.showDialog();
-				if (newQuestion != null)
-					listmodel.addElement(newQuestion);
-			}
-		});
-		btnNewButton.setBounds(567, 33, 191, 23);
-		frmQuizumMaker.getContentPane().add(btnNewButton);
+        JButton btnNewButton = new JButton("Nowe pytanie");
+        btnNewButton.addActionListener(e -> {
+            CreateEditDialog newQuestionDialog = new CreateEditDialog();
+            Question newQuestion = newQuestionDialog.showDialog();
+            if (newQuestion != null)
+                listmodel.addElement(newQuestion);
+        });
+        btnNewButton.setBounds(567, 33, 191, 23);
+        frmQuizumMaker.getContentPane().add(btnNewButton);
 
-		JButton btnNewButton_1 = new JButton("Edytuj");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Question selectedQuestion = list.getSelectedValue();
-				if (selectedQuestion != null) {
-					CreateEditDialog newQuestionDialog = new CreateEditDialog(selectedQuestion);
-					Question editedQuestion = newQuestionDialog.showDialog();
-					if (editedQuestion != null)
-						listmodel.setElementAt(editedQuestion, list.getSelectedIndex());
-				}
-			}
-		});
-		btnNewButton_1.setBounds(567, 101, 191, 23);
-		frmQuizumMaker.getContentPane().add(btnNewButton_1);
+        JButton btnNewButton_1 = new JButton("Edytuj");
+        btnNewButton_1.addActionListener(e -> {
+            Question selectedQuestion = list.getSelectedValue();
+            if (selectedQuestion != null) {
+                CreateEditDialog newQuestionDialog = new CreateEditDialog(selectedQuestion);
+                Question editedQuestion = newQuestionDialog.showDialog();
+                if (editedQuestion != null)
+                    listmodel.setElementAt(editedQuestion, list.getSelectedIndex());
+            }
+        });
+        btnNewButton_1.setBounds(567, 101, 191, 23);
+        frmQuizumMaker.getContentPane().add(btnNewButton_1);
 
-		JButton btnNewButton_2 = new JButton("Usuń");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				listmodel.removeElementAt(list.getSelectedIndex());
-			}
-		});
-		btnNewButton_2.setBounds(567, 135, 191, 23);
-		frmQuizumMaker.getContentPane().add(btnNewButton_2);
+        JButton btnNewButton_2 = new JButton("Usuń");
+        btnNewButton_2.addActionListener(e -> listmodel.removeElementAt(list.getSelectedIndex()));
+        btnNewButton_2.setBounds(567, 135, 191, 23);
+        frmQuizumMaker.getContentPane().add(btnNewButton_2);
 
-		JMenuBar menuBar = new JMenuBar();
-		frmQuizumMaker.setJMenuBar(menuBar);
+        JMenuBar menuBar = new JMenuBar();
+        frmQuizumMaker.setJMenuBar(menuBar);
 
-		JMenu mnPlik = new JMenu("Plik");
-		menuBar.add(mnPlik);
+        JMenu mnPlik = new JMenu("Plik");
+        menuBar.add(mnPlik);
 
-		JMenuItem mntmNowyZestaw = new JMenuItem("Nowy");
-		mntmNowyZestaw.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				listmodel.clear();
-			}
-		});
-		mnPlik.add(mntmNowyZestaw);
+        JMenuItem mntmNowyZestaw = new JMenuItem("Nowy");
+        mntmNowyZestaw.addActionListener(listener -> listmodel.clear());
+        mnPlik.add(mntmNowyZestaw);
 
-		JMenuItem mntmWczytajZestaw = new JMenuItem("Wczytaj");
-		mntmWczytajZestaw.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				listmodel.clear();
+        JMenuItem mntmWczytajZestaw = new JMenuItem("Wczytaj");
+        mntmWczytajZestaw.addActionListener(listener -> {
+            listmodel.clear();
 
-				fc.setCurrentDirectory(new File("."));
-				int returnVal = fc.showOpenDialog(frmQuizumMaker);
+            fc.setCurrentDirectory(new File("."));
+            int returnVal = fc.showOpenDialog(frmQuizumMaker);
 
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					jsonQuestionService.loadQuestionList(fc.getSelectedFile())
-							.forEach(question -> listmodel.addElement(question));
-				}
-			}
-		});
-		mnPlik.add(mntmWczytajZestaw);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
 
-		JMenuItem mntmZapisz = new JMenuItem("Zapisz");
-		mntmZapisz.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String filename = "";
-				
-				fc.setCurrentDirectory(new File("."));
-				fc.setFileFilter(new FileNameExtensionFilter("CSV", "csv"));
-				int returnVal = fc.showSaveDialog(frmQuizumMaker);
+                File selectedFile = fc.getSelectedFile();
+                if (selectedFile.getName().endsWith(".json")) {
+                    jsonQuestionService.loadQuestionList(selectedFile)
+                            .forEach(question -> listmodel.addElement(question));
+                } else {
+                    csvQuestionService.loadQuestionList(selectedFile)
+                            .forEach(question -> listmodel.addElement(question));
+                }
+            }
+        });
+        mnPlik.add(mntmWczytajZestaw);
 
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					filename = fc.getSelectedFile().getPath().replace("json","csv");
-				}
-				
-				if (filename.isEmpty())
-					return;
-				
-				csvQuestionService.writeQuestionList(new File(filename.contains(".csv") ? filename : filename+".csv"), Collections.list(listmodel.elements()));
-			}
-		});
-		mnPlik.add(mntmZapisz);
-	}
+        JMenuItem mntmZapiszJSON = new JMenuItem("Zapisz jako JSON");
+        mntmZapiszJSON.addActionListener(listener -> {
+            String filename = "";
+
+            fc.setCurrentDirectory(new File("."));
+            fc.setFileFilter(new FileNameExtensionFilter("JSON", "json"));
+            fc.setSelectedFile(new File("pytania.json"));
+            int returnVal = fc.showSaveDialog(frmQuizumMaker);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                filename = fc.getSelectedFile().getPath();
+            }
+
+            if (filename.isEmpty())
+                return;
+
+            csvQuestionService.writeQuestionList(new File(filename.endsWith(".json") ? filename : filename + ".json"), Collections.list(listmodel.elements()));
+        });
+        mnPlik.add(mntmZapiszJSON);
+
+        JMenuItem mntmZapiszCSV = new JMenuItem("Zapisz jako CSV");
+        mntmZapiszCSV.addActionListener(listener -> {
+            String filename = "";
+
+            fc.setCurrentDirectory(new File("."));
+            fc.setFileFilter(new FileNameExtensionFilter("CSV", "csv"));
+            fc.setSelectedFile(new File("pytania.csv"));
+            int returnVal = fc.showSaveDialog(frmQuizumMaker);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                filename = fc.getSelectedFile().getPath();
+            }
+
+            if (filename.isEmpty())
+                return;
+
+            csvQuestionService.writeQuestionList(new File(filename.endsWith(".csv") ? filename : filename + ".csv"), Collections.list(listmodel.elements()));
+        });
+        mnPlik.add(mntmZapiszCSV);
+    }
 }
